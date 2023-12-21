161a0056514bf126ce0f4632baa01c175768b39f
vorber <vorber@gmail.com>
2023-12-22 00:27:37 +0200

    added neotree, lualine

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return
 {
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
-	end,
-	config = function () 
-      		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "c", "c_sharp", "lua", "vim", "vimdoc", "javascript", "html" },
-			sync_install = false,
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },  
-			})
-	end
+        configs.setup({
+            ensure_installed = { "c", "c_sharp", "lua", "vim", "vimdoc", "javascript", "html" },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
