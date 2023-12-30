968172529a742d887aeca3a69d7a2e3b5ad71387
vorber <vorber@gmail.com>
2023-12-31 00:41:24 +0200

    git tweaks

--- nix/nixos/configuration.nix
+++ nix/nixos/configuration.nix
@@ -95,7 +95,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git
     curl
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
