fc68c34285c9f33c89111fb36f3e55c598ce26fd
vorber <vorber@gmail.com>
2024-02-29 23:51:52 +0200

    waybar initial config

--- nix/home/hyprland/default.nix
+++ nix/home/hyprland/default.nix
@@ -1,16 +1,8 @@
 {config, pkgs, lib, ...}:
-let
-  hypr = {
-    "enabled" = [
-      (import ./hyprland.nix)
-      (import ./waybar.nix)
-      (import ./rofi.nix)
-    ];
-    "disabled" = [];
-  };
-  setting = if config.isNixOS then "enabled" else "disabled";
-in
 {
   imports = [
-  ] ++ (hypr.${setting} or []);
+      ./hyprland.nix
+      ./waybar.nix
+      ./rofi.nix
+  ];
 }
