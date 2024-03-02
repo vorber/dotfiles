56f2a17d7fb20f4d6f4abcd873c09f368cb2c6e2
vorber <vorber@gmail.com>
2024-03-03 00:25:35 +0200

    add fuzzel

---
+++ nix/home/hyprland/launcher/fuzzel.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, lib, inputs, ... }:
+let
+  palette = config.colorScheme.palette;
+in
+{
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        terminal = "${pkgs.wezterm}/bin/wezterm";
+        layer = "overlay";
+        font = "MesloLGMNerdFontMono:size=14";
+        dpi-aware = "yes";
+      };
+      dmenu = {
+        exit-immediately-if-empty="yes";
+      };
+      border = {
+        width = 1;
+      };
+      colors = {
+        background="${palette.base00}dd";
+        text="${palette.base05}ff";
+        match="${palette.base08}ff";
+        selection="${palette.base04}ff";
+        selection-match="${palette.base08}ff";
+        selection-text="${palette.base05}ff";
+        border="${palette.base07}ff";
+      };
+    };
+  };
+}
