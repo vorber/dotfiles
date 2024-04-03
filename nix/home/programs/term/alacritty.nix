80aa5c6b672b99fffe4b710f5f504aa2067322b9
vorber <vorber@gmail.com>
2024-04-03 19:03:45 +0300

    alacritty config

--- nix/home/programs/term/alacritty.nix
+++ nix/home/programs/term/alacritty.nix
@@ -1,6 +1,30 @@
-{ pkgs, ...}:
+{ config, ...}:
+let
+  palette = config.colorScheme.palette;
+in
 {
   programs.alacritty = {
     enable = true;
+    settings = {
+      live_config_reload = true;
+      window = {
+        opacity = 0.75;
+        blur = true;
+      };
+      scrolling.history = 100000;
+      font = {
+        normal.family = "FiraCode Nerd Font";
+        bold = { style = "Bold"; };
+        size = 11;
+      };
+#      colors = {
+#        primary = {
+#          background = "#303446";
+#          foreground = "#C6D0F5";
+#          dim_foreground = "#C6D0F5";
+#          bright_foreground = "#C6D0F5";         
+#        };
+#      };
+    };
   };
 }
